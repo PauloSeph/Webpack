40cbec045bd9d777cf89918814ea2df1c5483d5d
Seph <paulo.seph@hotmail.com>
2019-12-14 21:37:02 -0300

    Desenvolvimento e produção

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,12 +1,26 @@
+const modoDev = process.env.NODE_ENV !== 'production'
 const webpack = require('webpack')
 const MiniCSS = require('mini-css-extract-plugin')
+const uglyfyJsPlugin = require('uglifyjs-webpack-plugin')
+const optimeCSS = require('optimize-css-assets-webpack-plugin')
+
+
 
 module.exports = {
-    mode: 'development',
+    mode: modoDev ? 'development' : 'production',
     entry: './src/principal.js',
     output: {
         filename: 'principal.js',
         path: __dirname + '/public'
+    },
+    optimization: {
+        minimizer: [
+            new uglyfyJsPlugin ({
+                cache: true,
+                parallel: true
+            }),
+            new optimeCSS({})
+        ]
     },
     plugins: [
             new MiniCSS({
